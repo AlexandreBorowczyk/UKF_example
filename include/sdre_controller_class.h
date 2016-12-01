bffe1fec74ae245699bb759e39118185c070ab7b
Alexandre Borowczyk <borowczyk.alexandre@gmail.com>
2016-11-30 22:43:31 -0500

    Initial Commit
    To be SDRE controller class.

---
+++ include/sdre_controller_class.h
@@ -0,0 +1,18 @@
+#ifndef SDRECONTROLLER_H_
+#define SDRECONTROLLER_H_
+
+class SdreController {
+    public:
+        explicit SdreController();
+        virtual ~SdreController();
+
+        void OnInit() ;
+
+        void OnUpdate();
+
+    private:
+
+};
+
+
+#endif  // SDRECONTROLLER_H_
---
+++ src/sdre_controller_class.cpp
@@ -0,0 +1,19 @@
+#include <sdre_controller_class.h>
+
+#include <stdio.h>
+
+SdreController::SdreController() {
+  // Do nothing
+}
+
+SdreController::~SdreController(){
+  // Do nothing
+}
+
+void SdreController::OnInit() {
+// Do nothing
+}
+
+void SdreController::OnUpdate() {
+// Do nothing
+}
