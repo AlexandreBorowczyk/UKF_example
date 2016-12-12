4502e366c4765d904a5cd6d8318aeeac3aa30d22
Alexandre Borowczyk <borowczyk.alexandre@gmail.com>
2016-12-12 18:30:02 -0500

    defined preliminary structure and started to write the ctrl code.

--- ukf_example/include/sdre_controller_class.h
+++ ukf_example/include/sdre_controller_class.h
@@ -5,18 +5,42 @@
 
 class SdreController {
     public:
-        explicit SdreController();
-        virtual ~SdreController();
+        explicit SdreController(
+                const double & cart_mass,
+                const double & pendulum1_mass,
+                const double & pendulum2_mass,
+                const double & pendulum1_length_,
+                const double & pendulum2_length_);
 
-        void OnInit() ;
+        ~SdreController();
 
-        void OnUpdate();
-
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-        private:
+        double ComputeCommand(
+                const Eigen::Matrix<double,6,1> & X,
+                const double & iteration_time);
 
 
+
+    private:
+
+        void ComputeCoef();
+
+        // System physical param.
+        double cart_mass_;
+        double pendulum1_mass_;
+        double pendulum2_mass_;
+        double pendulum1_length_;
+        double pendulum2_length_;
+
+        // Computed coef.
+        Eigen::Matrix<double,6,6> d_;
+        Eigen::Matrix<double,1,3> f_;
+
+
+        // Controller parameter
+        Eigen::Matrix<double,6,6> Q_;
+        double R_;
+
+    public : EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 
 
--- ukf_example/src/sdre_controller_class.cpp
+++ ukf_example/src/sdre_controller_class.cpp
@@ -4,18 +4,64 @@
 
 #include <eigen3/unsupported/Eigen/src/MatrixFunctions/MatrixExponential.h>
 
-SdreController::SdreController() {
-  // Do nothing
+SdreController::SdreController(
+    const double & cart_mass,
+    const double & pendulum1_mass,
+    const double & pendulum2_mass,
+    const double & pendulum1_length_,
+    const double & pendulum2_length_)
+  : cart_mass_(cart_mass),
+    pendulum1_mass_(pendulum1_mass),
+    pendulum2_mass_(pendulum2_mass),
+    pendulum1_length_(pendulum1_length),
+    pendulum2_length_(pendulum2_length) {
+  d_ = Eigen::Matrix<double,6,6>::Zero();
+  f_ = Eigen::Matrix<double,1,3>::Zero();
+
+  ComputeCoef();
 }
 
-SdreController::~SdreController(){
-  // Do nothing
+SdreController::~SdreController() {}
+
+void SdreController::ComputeCoef() {
+  d_(0,0) = cart_mass_ + pendulum1_mass_ + pendulum2_mass_;
+  d_(0,1) = ((0.5 * pendulum1_mass_) + pendulum2_mass_) * pendulum1_length_;
+  d_(0,2) = 0.5 * pendulum2_mass_ * pendulum2_length_;
+  d_(1,1) = ((pendulum1_mass_ / 3.0) + pendulum2_mass_) * std::pow(pendulum1_length_,2.0);
+  d_(1,2) = 0.5 * pendulum2_mass_ * pendulum1_length_ * pendulum2_length_;
+  d_(3,3) = std::pow(pendulum2_length_,2.0) * pendulum2_mass_ / 3.0;
+  d_(1,0) = d_(0,1);
+  d_(2,0) = d_(0,2);
+  d_(2,1) = d_(1,2);
+
+  f_(1) = ((0.5 * pendulum1_mass_) + pendulum2_mass_) * pendulum1_length_ * 9.81;
+  f_(2) = 0.5 * pendulum2_mass_ * pendulum2_length_ * 9.81;
 }
 
-void SdreController::OnInit() {
-// Do nothing
-}
+double SdreController::ComputeCommand(
+    const Eigen::Matrix<double, _Tp2, _Tp3> & X,
+    const double & iteration_time) {
 
-void SdreController::OnUpdate() {
-// Do nothing
+double cos_theta1 = std::cos(X(1));
+double cos_theta2 = std::cos(X(2));
+double cos_delta_thetas = std::cos(X(1)-X(2));
+
+double sin_theta1 = std::sin(X(1));
+double sin_theta2 = std::sin(X(2));
+double sin_delta_thetas = std::sin(X(1)-X(2));
+
+Eigen::Matrix<double,3,3> D = d_;
+D(0,1) *= cos_theta1;
+D(0,2) *= cos_theta2;
+D(1,2) *= cos_delta_thetas;
+D(1,0) = D(0,1);
+D(2,0) = D(0,2);
+D(2,1) = D(1,2);
+
+Eigen::Matrix<double,3,3> C = Eigen::Matrix<double,3,3>::Zero();
+C(0,1) = -1.0 * d_(0,1) * sin_theta1 * X(5);
+C(0,2) = -1.0 * d_(0,2) * sin_theta2 * X(6);
+C(1,2) =  d_(1,2) * sin_delta_thetas * X(6);
+C(2,1) =  d_(2,1) * sin_delta_thetas * X(5);
+
 }
